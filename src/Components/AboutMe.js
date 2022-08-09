4e676572397d77a576c319c5ce2141e650d6db46
eduardbede <bede.eduard@gmail.com>
2022-08-09 11:40:32 +0300

    Update CSS

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,6 +1,9 @@
 import { GoTriangleRight } from "react-icons/go";
 import { motion } from "framer-motion";
-
+import { AiFillHtml5 } from "react-icons/ai";
+import { FaCss3Alt, FaReact, FaGitAlt, FaGithub } from "react-icons/fa";
+import { SiJavascript, SiFramer } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
 
 export default function AboutMe(props){
     const firstName = "Bede";
@@ -12,10 +15,49 @@ export default function AboutMe(props){
 
     const technologiiText = `Here are some technologies that I learned:`;
 
-    const skills=['HTML',"CSS",'JavaScript(ES6)','React.JS','Tailwind CSS', 'Git Version-Control', 'Framer Motion library'];
+    const skills=[{
+                    skill: 'HTML',
+                    icon : <AiFillHtml5 size={'1.5em'} color={"#DD4B25"}/>
+                    },
+
+                    {
+                    skill: "CSS",
+                    icon : <FaCss3Alt size={'1.5em'} color={"#254BDD"}/>
+                    },
+                    
+                    {
+                    skill: 'JavaScript(ES6)',
+                    icon : <SiJavascript size={'1.5em'} color={"#EFD81D"}/>
+                    },
+                    
+                    {
+                    skill: 'React.JS',
+                    icon : <FaReact size={'1.5em'} color={"#5FD6F6"}/>
+                    },
+                    
+                    {
+                    skill: 'Tailwind CSS',
+                    icon : <SiTailwindcss size={'1.5em'} color={"#38BDF8"}/>
+                    },
+                     
+                    {
+                    skill: 'Git Version-Control',
+                    icon : <FaGitAlt size={'1.5em'} color={"#E84E31"}/>
+                    },
+                    
+                    {
+                    skill: 'GitHub',
+                    icon : <FaGithub size={'1.5em'} color={"#ffffff"}/>
+                    },
+                    
+                    {
+                    skill: 'Framer Motion library',
+                    icon : <SiFramer size={'1.5em'} color={"#DD369C"}/>
+                    }
+        ];
 
     const mapSkills = skills.map((el, index)=>{
-        return <li className="text-myGrey" key={index}><GoTriangleRight color={"#60f7d3"}/>{el}</li>
+        return <li className="text-myGrey" key={index}><GoTriangleRight color={"#60f7d3"}/>{el.icon}{el.skill}</li>
     })
 
     return(
@@ -52,7 +94,7 @@ export default function AboutMe(props){
                         whileInView={{y:0, opacity: 1 }}
                         transition={{duration:0.5 ,delay:0.6}}
                         viewport={{ once: true }}
-                        className="skillsDiv pt-5 ">
+                        className="skillsDiv pt-5">
                     {mapSkills}
                 </motion.ul>
             </div>
